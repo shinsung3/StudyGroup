87a074bb97346d9b3125feda5f4d7120b6838f2a
nustar <tlstjd436@naver.com>
2021-06-04 17:52:00 +0900

    #6 - 장고 업로드 : 전반적인 레이아웃 잡기

--- djangoProject/app/views.py
+++ djangoProject/app/views.py
@@ -4,6 +4,6 @@ from .models import MyStudy
 # Create your views here.
 def home_list(request):
     # 어학 스터디 모임만 가져올 수 있음
-    studyInfo = MyStudy.objects.filter(category="어학")
+    studyInfo = MyStudy.objects.filter()
     return render(request, 'app/home_list.html', {'studyList':studyInfo})
     # info 라는 이름은 그냥 아무거나 상관없음. - model 데이터 넘겨준것뿐.

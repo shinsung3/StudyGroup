859c0219fd09f93946fd29a94767ebba82328a00
nustar <tlstjd436@naver.com>
2021-06-05 16:53:14 +0900

    #8 - 컨텐츠 리스트 꾸미기

--- djangoProject/app/urls.py
+++ djangoProject/app/urls.py
@@ -3,5 +3,6 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('', views.home_list, name='home_list')
+    path('', views.home_list, name='home_list'),
+    path('study_list/', views.study_list, name='study_list')
 ]
--- djangoProject/app/views.py
+++ djangoProject/app/views.py
@@ -7,3 +7,13 @@ def home_list(request):
     studyInfo = MyStudy.objects.filter()
     return render(request, 'app/home_list.html', {'studyList':studyInfo})
     # info 라는 이름은 그냥 아무거나 상관없음. - model 데이터 넘겨준것뿐.
+
+def study_list(request):
+    studyInfo = MyStudy.objects.filter()
+    language_study = MyStudy.objects.filter(category="어학")
+    job_study = MyStudy.objects.filter(category="취업")
+    school_study = MyStudy.objects.filter(category="학습")
+    etc_study = MyStudy.objects.filter(category="기타")
+    return render(request, 'app/study_list.html', {'studyList':studyInfo,
+    'languageList':language_study,'jobList':job_study
+    ,'schoolList':school_study,'etcList':etc_study})
